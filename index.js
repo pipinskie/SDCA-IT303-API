f0a02a42c2f531857cee024bdb5f0c10cabda7a2
Prince Jin Stephen Moya <Princejinstephenmoya@gmail.com>
2025-09-06 10:14:27 +0800

    Add initial endpoint

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express();
+
+app.use('/api/register', (req, res) => {
+    res.send('Sample register endpoint')
+})
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000.')
+})
